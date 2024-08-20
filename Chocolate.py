778c89ae8bcee2184db5510c336ef4ebacba37cc
Juancyn <alejandrovera_05@hotmail.com>
2024-08-20 10:04:01 -0500

    Creación de index.html (Hasta min 4:46)
    :D

--- Chocolate.py
+++
@@ -1,8 +0,0 @@
-print("Dame un pedazo de ese chocolate");
-print("Se acabo ese era el ultimo");
-print("Pero tenia tantas ganas de comerlo");
-print("Enserio?")
-print("Enserio")
-print("No te defraudare papa, hare el mejor chocolate casero")
-print("Me cree?")
-print("Solo si lo veo")
